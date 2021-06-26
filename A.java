027ed92123c2a81620f7db33be2af478715990f5
Shivam Gupta <sg247938@gmail.com>
2021-06-26 13:50:08 +0530

    Add files via upload

---
+++ A.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class A
+{
+ 
+    void display(int n)
+    {
+    
+    for (int i = 0; i<=n; i++) {
+    
+    for (int j = 0; j<= n / 2; j++) {
+    
+    if ((j == 0 || j == n / 2) && i != 0 || i == 0  && j != n / 2 || i == n / 2)
+    System.out.print("*");
+    else
+    System.out.print(" ");
+    }
+    System.out.println();
+    }
+    }
+    public static void main(String[] args)
+    {
+    Scanner sc = new Scanner(System.in);
+    A a = new A();
+    a.display(7);
+    }
+    }
